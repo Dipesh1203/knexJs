c42d0d4659e6371af21601b28209134d514f1d9e
Dipesh1203 <prajapatdipesh86@gmail.com>
2024-01-05 00:24:06 +0530

    changes and deleted some files

--- app.js
+++ app.js
@@ -1,8 +1,30 @@
 const express = require("express");
 const app = express();
-const router = require("./routes");
+const knex = require("./db/db.js");
 
-app.use(router);
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.post("/user", (req, res) => {
+  knex("person")
+    .insert({
+      email: "rajeshjain211@gmail.com",
+      first_name: "Rajesh",
+      last_name: "Jain",
+    })
+    .then(() => {
+      knex
+        .select()
+        .from("person")
+        .then((users) => {
+          console.log(users);
+          res.send(users);
+        });
+    });
+});
 
 app.listen(8080, () => {
   console.log("listening to port");
