cf9d6d79450bfa3117b166c10de4e549a388267a
Dipesh1203 <prajapatdipesh86@gmail.com>
2024-01-14 19:16:26 +0530

    show route added

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const app = express();
 const db = require("./db/db.js");
+const path = require("path");
+const ejsMate = require("ejs-mate");
 const bodyParser = require("body-parser");
 const personController = require("./controller/person.js");
 const router = require("./routes/person.js");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "/public")));
 
 // app.use(express.json());
 app.use(bodyParser.json());
--- routes/person.js
+++ routes/person.js
@@ -1,5 +1,7 @@
 const express = require("express");
+const db = require("../db/db");
 const personController = require("../controller/person.js");
+const { createPerson } = require("../dao/person.js");
 
 const router = express.Router();
 
@@ -7,6 +9,17 @@ router.get("/new", (req, res) => {
   res.render("person/person.ejs");
 });
 
-router.post("/", personController.createPerson);
+//create route
+router.post("/", (req, res) => {
+  personController.createPerson;
+  res.redirect("person/show");
+});
+
+//show route
+router.get("/show", async (req, res) => {
+  let tableData = await db.select().from("person");
+  console.log(tableData);
+  res.render("person/index.ejs", { tableData });
+});
 
 module.exports = router;
